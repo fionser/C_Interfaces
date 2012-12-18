a313c1275e38efbb2cbb8589a6613f3c7c98dff4
fionser <fionser@gmail.com>
2012-12-18 15:19:38 +0800

    More effective

--- arith.c
+++ arith.c
@@ -1,12 +1,12 @@
 #include "arith.h"
 int Arith_max(int x, int y)
 {
-	return x > y ? x : y;
+        return x ^ ((x ^ y) & -(x < y));
 }
 
 int Arith_min(int x, int y)
 {
-	return x < y ? x : y;
+        return y ^ ((x ^ y) & -(x < y));
 }
 
 int Arith_div(int x, int y)
